81fe2e69277b5700406b4b936a69d837189be49f
sandeep-gitdev <sandeepdhanger101@gmail.com>
2024-10-18 09:20:26 +0530

    Calcu_notes

---
+++ calcu_notes.js
@@ -0,0 +1,18 @@
+// Calulate minimum number for notes
+
+let notes = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+let amount = 1111;
+let result = {};
+
+  for (let i = 0; i < notes.length; i++) {
+     if (amount >= notes[i]) {
+        let count = Math.floor(amount / notes[i]);
+        amount -= count * notes[i];
+        if (count > 0) {
+            result[notes[i]] = count;
+        }    
+     }
+    
+  }
+
+  console.log(result);
